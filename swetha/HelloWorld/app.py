8b1d702b37422631b7565d487e7001c2aeb777e7
Vijayalakshmi Kaladindi <iswetha522@gmail.com>
2019-03-18 13:08:55 +0530

    Created Helloworld, Variables and Strings folders

---
+++ swetha/HelloWorld/app.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+print("*" * 10)
+print("Hello World!")
+2 + 3
+x = 1
+y = 2
+unit_price = 3
---
+++ swetha/Variables/app.py
@@ -0,0 +1,27 @@
+students_count = 1000
+rating = 4.99
+is_published = False
+# Escape Sequences
+# \"
+# \'
+# \\
+# \n (new line)
+# \t (tab)
+course_name = "Python \"Programming"
+print(course_name)
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])
+print(students_count)
+print(is_published)
+
+first = "Vijayalakshmi"
+last = "Kalidindi"
+full = first + " " + last
+formatted_full = f"{first} {last} {len(full)}"
+print(full)
+print(formatted_full)
