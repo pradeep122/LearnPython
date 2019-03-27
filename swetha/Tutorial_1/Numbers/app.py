b7272ffbe9b67c2f873758aacf90667011a254b3
Vijayalakshmi Kaladindi <iswetha522@gmail.com>
2019-03-27 12:53:29 +0530

    Created a folder tutorial_1 and added the previous code to it

--- swetha/Tutorial_1/Numbers/app.py
+++ swetha/Tutorial_1/Numbers/app.py
@@ -2,18 +2,18 @@ import math
 x = 1
 x = 1.1
 x = 1 + 2j    # a+ bi
-print(10 + 3)
-print(10 - 3)
-print(10 * 3)
-print(10 / 3)  # quotient
-print(10 // 3)
-print(10 % 3)  # remainder
-print(10 ** 3)
+print(10 + 3)  # addition operator
+print(10 - 3)  # subtraction operator
+print(10 * 3)  # multiplication operator
+print(10 / 3)  # division operator returns the quotient with floating number
+print(10 // 3)  # division operator returns the quotient with integer number
+print(10 % 3)  # modulus operator returns remainder
+print(10 ** 3)  # exponent operator returns 10 to the "power of" 3
 
 
 x = 10
 # x = x + 3
-x += 3
+x += 3  # augument assignment operator
 
 print(round(2.9))
 print(abs(-2.9))
--- swetha/Tutorial_1/Variables/app.py
+++ swetha/Tutorial_1/Variables/app.py
@@ -25,17 +25,3 @@ full = first + " " + last
 formatted_full = f"{first} {last} {len(full)}"
 print(full)
 print(formatted_full)
-
-
-# Python interprets the code line by line
-price = 10  # (initial value)
-price = 20  # (reset Value)
-print(price)
-
-# exercise for variables
-full_name = "John Smith"
-age = 18
-is_new_patient = True
-name = input("What is your name? ")
-color = input("what is your favourite color? ")
-print(name + " likes " + color)
