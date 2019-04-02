72d245ef6abf4a0b61b829dfbd8f2411789b4593
Pradeep Dantuluri <pradeep@yantranet.com>
2019-04-03 02:19:31 +0530

    making excel code maintianable

--- pradeep/tutorial2/projects/xl_processor/app.py
+++ pradeep/tutorial2/projects/xl_processor/app.py
@@ -1,31 +1,35 @@
-import openpyxl as xl
-from openpyxl.chart import BarChart, Reference
 
-workbook = xl.load_workbook(
-    'pradeep/tutorial2/projects/xl_processor/transactions.xlsx')
+input_file = 'pradeep/tutorial2/projects/xl_processor/transactions.xlsx'
+output_file = 'pradeep/tutorial2/projects/xl_processor/corrected_transactions.xlsx'
 
-sheet1 = workbook['Sheet1']
-cell = sheet1['a1']
-cell = sheet1.cell(1, 1)
 
-print(cell.value)
+def process_spreadsheet(input_file, output_file):
+    import openpyxl as xl
+    from openpyxl.chart import BarChart, Reference
 
-for row in range(2, sheet1.max_row + 1):
-    cell = sheet1.cell(row, 3)
+    workbook = xl.load_workbook(input_file)
+
+    sheet = workbook['Sheet']
+    cell = sheet['a1']
+    cell = sheet.cell(1, 1)
+
     print(cell.value)
-    corrected_value = cell.value * 0.9
-    corrected_price_cell = sheet1.cell(row, 4)
-    corrected_price_cell.value = corrected_value
 
-values = Reference(sheet1,
-                   min_row=2,
-                   max_row=sheet1.max_row,
-                   min_col=4,
-                   max_col=4)
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        print(cell.value)
+        corrected_value = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_value
 
-chart1 = BarChart()
-chart1.add_data(values)
-sheet1.add_chart(chart1)
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
 
-workbook.save(
-    'pradeep/tutorial2/projects/xl_processor/corrected_transactions.xlsx')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart)
+
+    workbook.save(output_file)
